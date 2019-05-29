a5a96b1afcc2a23775e5e6661782184736b03b3a
Ben Amara Amir <amir.benamara@esprit.tn>
2019-05-29 20:47:14 +0100

    first commit

---
+++ src/AppBundle/Controller/FilmController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Film;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Film controller.
+ *
+ * @Route("film")
+ */
+class FilmController extends Controller
+{
+    /**
+     * Lists all film entities.
+     *
+     * @Route("/", name="film_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $films = $em->getRepository('AppBundle:Film')->findAll();
+
+        return $this->render('film/index.html.twig', array(
+            'films' => $films,
+        ));
+    }
+
+    /**
+     * Creates a new film entity.
+     *
+     * @Route("/new", name="film_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $film = new Film();
+        $form = $this->createForm('AppBundle\Form\FilmType', $film);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($film);
+            $em->flush();
+
+            return $this->redirectToRoute('film_show', array('idFilm' => $film->getIdfilm()));
+        }
+
+        return $this->render('film/new.html.twig', array(
+            'film' => $film,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a film entity.
+     *
+     * @Route("/{idFilm}", name="film_show")
+     * @Method("GET")
+     */
+    public function showAction(Film $film)
+    {
+        $deleteForm = $this->createDeleteForm($film);
+
+        return $this->render('film/show.html.twig', array(
+            'film' => $film,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing film entity.
+     *
+     * @Route("/{idFilm}/edit", name="film_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Film $film)
+    {
+        $deleteForm = $this->createDeleteForm($film);
+        $editForm = $this->createForm('AppBundle\Form\FilmType', $film);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('film_edit', array('idFilm' => $film->getIdfilm()));
+        }
+
+        return $this->render('film/edit.html.twig', array(
+            'film' => $film,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a film entity.
+     *
+     * @Route("/{idFilm}", name="film_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Film $film)
+    {
+        $form = $this->createDeleteForm($film);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($film);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('film_index');
+    }
+
+    /**
+     * Creates a form to delete a film entity.
+     *
+     * @param Film $film The film entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Film $film)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('film_delete', array('idFilm' => $film->getIdfilm())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Film.php
@@ -0,0 +1,281 @@
+<?php
+
+namespace AppBundle\Entity;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints\Date;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Film
+ *
+ * @ORM\Table(name="film")
+ * @Vich\Uploadable
+ * @ORM\Entity
+ */
+class Film
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="Id_film", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idFilm;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255, nullable=false)
+     */
+    private $titre;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="entree_total", type="integer" , nullable=true)
+     */
+    private $entree_total;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="part_dist_salle", type="float" ,nullable=true)
+     */
+    private $part_dist_salle;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="part_dist_region", type="float" ,nullable=true)
+     */
+     private $part_dist_region;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="part_prod_salle", type="float" ,nullable=true)
+     */
+    private $part_prod_salle;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="part_prod_region", type="float" , nullable=true)
+     */
+    private $part_prod_region;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="depenses_total", type="float" , nullable=true)
+     */
+    private $depenses_total;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="Image_Astuce", type="date")
+     */
+    private $date_sortie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @return int
+     */
+    public function getIdFilm()
+    {
+        return $this->idFilm;
+    }
+
+    /**
+     * @param int $idFilm
+     */
+    public function setIdFilm($idFilm)
+    {
+        $this->idFilm = $idFilm;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param string $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEntreeTotal()
+    {
+        return $this->entree_total;
+    }
+
+    /**
+     * @param int $entree_total
+     */
+    public function setEntreeTotal($entree_total)
+    {
+        $this->entree_total = $entree_total;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPartDistSalle()
+    {
+        return $this->part_dist_salle;
+    }
+
+    /**
+     * @param float $part_dist_salle
+     */
+    public function setPartDistSalle($part_dist_salle)
+    {
+        $this->part_dist_salle = $part_dist_salle;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPartDistRegion()
+    {
+        return $this->part_dist_region;
+    }
+
+    /**
+     * @param float $part_dist_region
+     */
+    public function setPartDistRegion($part_dist_region)
+    {
+        $this->part_dist_region = $part_dist_region;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPartProdSalle()
+    {
+        return $this->part_prod_salle;
+    }
+
+    /**
+     * @param float $part_prod_salle
+     */
+    public function setPartProdSalle($part_prod_salle)
+    {
+        $this->part_prod_salle = $part_prod_salle;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPartProdRegion()
+    {
+        return $this->part_prod_region;
+    }
+
+    /**
+     * @param float $part_prod_region
+     */
+    public function setPartProdRegion($part_prod_region)
+    {
+        $this->part_prod_region = $part_prod_region;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDepensesTotal()
+    {
+        return $this->depenses_total;
+    }
+
+    /**
+     * @param float $depenses_total
+     */
+    public function setDepensesTotal($depenses_total)
+    {
+        $this->depenses_total = $depenses_total;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateSortie()
+    {
+        return $this->date_sortie;
+    }
+
+    /**
+     * @param \DateTime $date_sortie
+     */
+    public function setDateSortie($date_sortie)
+    {
+        $this->date_sortie = $date_sortie;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+
+
+    public function setImageFile(File $image)
+    {
+        $this->imageFile = $image;
+
+
+    }
+
+    public function __toString()
+    {
+        return $this->getTitre();
+    }
+
+
+}
+
