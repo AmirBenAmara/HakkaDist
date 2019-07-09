26edc54d68332da2817a4fdcf9af7fab6fa656eb
DhifMlak <Dhifmlak@gmail.com>
2019-07-09 10:47:48 +0100

    added registration

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mlak
+ * Date: 7/7/2019
+ * Time: 12:47 PM
+ */
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('roles', ChoiceType::class, [
+            'choices' => [
+                'ROLE_USER' => 'ROLE_USER',
+                'ROLE_ADMIN' => 'ROLE_ADMIN',
+                'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN',
+            ],
+            'required'  => true,
+            'multiple' => true
+        ]);
+    }
+
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+
+
+}
